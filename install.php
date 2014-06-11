a9767db44eb81ae0d96f969925cb6f96eb9a8577
Laszlo Elek, SJ <eleklaszlosj@gmail.com>
2014-06-11 14:52:13 +0200

    install.php

--- install.php
+++ install.php
@@ -1,24 +1,24 @@
 <?php
 if (!is_dir('fajlok')) {
     exec('ln -s ../fajlok fajlok',$em);
-    if (!is_dir('fajlok')) {
+    /*if (!is_dir('fajlok')) {
         exec('rm fajlok');
         mkdir('fajlok'); 
         mkdir('fajlok/templomok'); 
-    }
+    }*/
 }
 if (!is_dir('kepek')) {
     exec('ln -s ../kepek kepek',$em);
-    if (!is_dir('kepek')) {
+    /*if (!is_dir('kepek')) {
         mkdir('kepek'); 
         mkdir('kepek/templomok'); 
-    }
+    }*/
 }
-
+/*
 if (!is_dir('fajlok/igenaptar')) {
     mkdir('fajlok/igenaptar'); 
 }
-
+*/
 
 
 ?>
