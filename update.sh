ae82a3bff1f187f263972ba1d17fa543b4462c64
borazslo <eleklaszlosj@gmail.com>
2015-12-04 21:41:22 -0500

    travis (ssh remote, again)

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 branch=$1
 cd /home/miserend_hu_git/github/borazslo/$branch.miserend.hu
-git checkout $branch"
-pull origin $branch"
+git checkout $branch
+pull origin $branch
 php composer.phar install
 php migration.php
