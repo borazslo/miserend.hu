f8c0ccfa4770f8d8936931907312ef9668096537
borazslo <eleklaszlosj@gmail.com>
2015-12-05 10:51:14 -0500

    travis again
    phpunit exits with 2 on travis (locally fine)

--- load.php
+++ load.php
@@ -3,7 +3,11 @@ session_start();
 
 $vars = array();
 
-require 'vendor/autoload.php';
+if (!@include __DIR__ . '/vendor/autoload.php') {
+    die('You must set up the project dependencies, run the following commands:
+        wget http://getcomposer.org/composer.phar
+        php composer.phar install');
+}
 
 include_once('functions.php');
 include_once('config.php');
