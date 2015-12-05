c694cd6214f53c0242463e4c87292411d93139d1
borazslo <eleklaszlosj@gmail.com>
2015-12-05 11:15:21 -0500

    travis phpunit

--- mysql_sample.sql
+++ mysql_sample.sql
@@ -1014,7 +1014,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (10,'vacskamati','$2y$10$EsjnJE0dHH/H4P7PjB0ks.9w9CcCqkz8SJav/1etPgLos2b/rO7j.','hirek-szavazas-galeria-info-hirek-user-nevnaptar-reklam-miserend-igenaptar','i','*vendeg*','2011-02-23 19:52:06','2015-03-18 22:29:29','2015-03-18 23:52:50','egyik@gmail.com','','Lázár Ervin','','n',1);
+INSERT INTO `user` VALUES (10,'vacskamati','$2y$10$RmePrAkA68lNiKS9zY9yUuCdw6L7pw4ITysPe2MsBJ3QEqJKVz5V2','hirek-szavazas-galeria-info-hirek-user-nevnaptar-reklam-miserend-igenaptar','i','*vendeg*','2011-02-23 19:52:06','2015-03-18 22:29:29','2015-03-18 23:52:50','egyik@gmail.com','','Lázár Ervin','','n',1);
 INSERT INTO `user` VALUES (11,'manyok','$2y$10$bWzPPXw3uGTbxLhvnVGTOu3S74CxjwpSNba2Vin/klQys6A1ufECG','','i','*vendeg*','2012-12-22 17:24:19','2015-02-03 11:10:39',NULL,'email@cim.com','Manyók','Hétszünyükapanyányimanyók','','n',0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
