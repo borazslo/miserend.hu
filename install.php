12aa0ab543587d4e27721f08d0035af6f02bf411
borazslo <eleklaszlosj@gmail.com>
2015-12-05 10:01:19 -0500

    travis, install.php

--- install.php
+++ install.php
@@ -1,30 +1,34 @@
 <?php
-require_once('functions.php');
-require_once('config.php');
 
-exec('mysql -h "'.$config['connection']['host'].'" -u "'.$config['connection']['user'].'" --password="'.$config['connection']['password'].'" "'.$config['connection']['database'].'" < mysql_sample.sql');
+require_once('load.php');
 
+use Illuminate\Database\Capsule\Manager as DB;
+
+$result = DB::table('information_schema.tables')
+        ->where('table_type', "=", 'BASE TABLE')
+        ->where('table_schema', '=', $config['connection']['database'])
+        ->count();
+if (!$result) {
+    exec('mysql -h "' . $config['connection']['host'] . '" -u "' . $config['connection']['user'] . '" --password="' . $config['connection']['password'] . '" "' . $config['connection']['database'] . '" < mysql_sample.sql');
+}
 if (!is_dir('fajlok')) {
     //exec('ln -s ../fajlok fajlok',$em);
     if (!is_dir('fajlok')) {
         exec('rm fajlok');
-        mkdir('fajlok'); 
-        mkdir('fajlok/templomok'); 
+        mkdir('fajlok');
+        mkdir('fajlok/templomok');
     }
 }
 if (!is_dir('kepek')) {
     //exec('ln -s ../kepek kepek',$em);
     if (!is_dir('kepek')) {
-        mkdir('kepek'); 
-        mkdir('kepek/templomok'); 
-        mkdir('fajlok/staticmaps'); 
+        mkdir('kepek');
+        mkdir('kepek/templomok');
+        mkdir('fajlok/staticmaps');
     }
 }
 
 if (!is_dir('fajlok/igenaptar')) {
-    mkdir('fajlok/igenaptar'); 
+    mkdir('fajlok/igenaptar');
 }
-
-
-
 ?>
