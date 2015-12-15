56d72a741d725e7d7c81c64ea340105eafe44fce
borazslo <eleklaszlosj@gmail.com>
2015-12-14 21:25:10 -0500

    update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 branch=$1
 cd /home/miserend_hu_git/github/borazslo/$branch.miserend.hu
 git checkout $branch
-pull origin $branch
+git pull origin $branch
 php composer.phar install
 php migration.php
