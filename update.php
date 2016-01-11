c6d57df2c762f14a51f1ed123adf107979f5ef8a
borazslo <eleklaszlosj@gmail.com>
2016-01-10 23:55:44 -0500

    again

--- update.php
+++ update.php
@@ -2,10 +2,10 @@
 
 include_once 'load.php';
 
-$users = Illuminate\Database\Capsule\Manager::table('user')->select('uid','login','jelszo')->whereRaw('CHAR_LENGTH(`jelszo`) <> 60')->take(5)->orderBy('uid')->get();
+$users = Illuminate\Database\Capsule\Manager::table('user')->select('uid','login','jelszo')->whereRaw('CHAR_LENGTH(`jelszo`) <> 60')->orderBy('uid')->get();
 foreach($users as $user) {        
         $jelszo = password_hash(base64_decode($user->jelszo), PASSWORD_BCRYPT);
-        echo $user->login.": ".base64_decode($user->jelszo)." ->".$jelszo."<br/>";
+        echo $user->login.": ".$jelszo."<br/>";
         Illuminate\Database\Capsule\Manager::table('user')->where('uid',$user->uid)->update(['jelszo' => $jelszo]);    
 }
 
