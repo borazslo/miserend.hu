eaf5b670c8da221b8bce65a7d9a9781b193e71ef
borazslo <eleklaszlosj@gmail.com>
2021-02-11 23:14:28 +0100

    maps: popup thumbnail

--- classes/html/ajax/churchesinbbox.php
+++ classes/html/ajax/churchesinbbox.php
@@ -15,10 +15,15 @@ class ChurchesInBBox extends Ajax {
         $churchesInBBox = \Eloquent\Church::inBBox(['latMin'=>$bbox[0],'lonMin'=>$bbox[1],'latMax'=>$bbox[2],'lonMax'=>$bbox[3]])->get();
 
         $return = [];
-        foreach($churchesInBBox as $church) {
+        foreach($churchesInBBox as $church) {            
+            $church->photos;
+            if (isset($church->photos[0])) $thumbnail = $church->photos[0]->smallUrl;
+            else $thumbnail = false;
+                            
             $return[] = [
                 'id' => $church->id,
                 'nev' => $church->nev,
+                'thumbnail' => $thumbnail,
                 'lat'=> $church->location->lat,
                 'lon'=> $church->location->lon              
             ];
