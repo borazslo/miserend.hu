eee53e03792ce2682f55a1c9f46addddd7b21fdc
borazslo <eleklaszlosj@gmail.com>
2021-02-14 23:22:09 +0100

    Update autocompleteevents.php
    #29

--- classes/html/ajax/autocompleteevents.php
+++ classes/html/ajax/autocompleteevents.php
@@ -2,22 +2,21 @@
 
 namespace Html\Ajax;
 
+use Illuminate\Database\Capsule\Manager as DB;
+
 class AutocompleteEvents extends Ajax {
 
     public function __construct() {
-        if ($_REQUEST['text'] == '' OR preg_match('/^[0-9]{1}/i', $_REQUEST['text'])) {
+        $text = \Request::Text('text');
+        if ($text == '' OR preg_match('/^[0-9]{1}/i', $text)) {
             $return[] = array('label' => '<i>hónap és nap (hh-nn)</i>', 'value' => date('m-d'));
             $return[] = array('label' => '<i>pontos dátum (éééé-hh-nn)</i>', 'value' => date('Y-m-d'));
             $return[] = array('label' => '<i>vagy megfelelő kifejezés</i>', 'value' => '');
         }
 
-        $query = "SELECT name FROM events WHERE name  LIKE '%" . $_REQUEST['text'] . "%' GROUP BY name ORDER BY name LIMIT 8";
-        if (!$lekerdez = mysql_query($query))
-            $return[] = array('label' => 'hiba', 'value' => "HIBA az esemény keresőben!<br>$query<br>" . mysql_error());
-        while ($row = mysql_fetch_row($lekerdez, MYSQL_ASSOC)) {
-            $return[] = array('label' => $row['name'], 'value' => $row['name']);
-        }
+        $return = DB::table('events')->select('name as label','name as value')->where('name','LIKE','%'.$text.'%')->groupBy('name')->get();
         $this->content = json_encode(array('results' => $return));
     }
 
 }
+ 
