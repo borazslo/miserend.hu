8a9876a5df3fc56dbdff1afc89ec2f1e0649477f
borazslo <eleklaszlosj@gmail.com>
2021-03-06 10:05:16 +0100

    Update autocompletename.php
    #29

--- classes/html/ajax/autocompletename.php
+++ classes/html/ajax/autocompletename.php
@@ -2,30 +2,37 @@
 
 namespace Html\Ajax;
 
+use Illuminate\Database\Capsule\Manager as DB;
+
 class AutocompleteName extends Ajax {
 
     public function __construct() {
-        $text = sanitize($_REQUEST['text']);
-
-        $query = "SELECT idoszamitas, tol, ig FROM misek WHERE torles = '0000-00-00 00:00:00' AND idoszamitas LIKE '%" . $text . "%' ";
-        if ($_REQUEST['type'] == 'period')
-            $query .= " AND tmp_datumtol <> tmp_datumig ";
-        elseif ($_REQUEST['type'] == 'particular')
-            $query .= " AND tmp_datumtol = tmp_datumig ";
-        $query .= " GROUP BY idoszamitas ORDER BY idoszamitas LIMIT 10";
-
-        if (!$lekerdez = mysql_query($query)) {
-            $this->content = "HIBA az időszak keresőben!<br>$query<br>" . mysql_error();
-            return;
-        }
-        while ($row = mysql_fetch_row($lekerdez, MYSQL_ASSOC)) {
-            preg_match('/^(.*?)( -[0-9]{1,3}| \+[0-9]{1,3}|)$/', $row['tol'], $from);
-            preg_match('/^(.*?)( -[0-9]{1,3}| \+[0-9]{1,3}|)$/', $row['ig'], $to);
+        $text = \Request::TextRequired('text');
+        $type = \Request::InArrayRequired('type',['period','particular']);
+        
+        $query = DB::table('misek')
+                    ->select('idoszamitas','tol','ig')
+                    ->where('torles','0000-00-00 00:00:00')
+                    ->where('idoszamitas','LIKE','%'.$text.'%');
+        if ($type == 'period')
+            $query = $query->where('tmp_datumtol','<>','tmp_datumig');
+        elseif ($type == 'particular')
+            $query = $query->where('tmp_datumtol','tmp_datumig');
+           
+        $results = $query->groupBy('idoszamitas')
+                ->orderBy('idoszamitas')
+                ->limit(10)
+                ->get();
+                
+        $return = [];
+        foreach($results as $row) {
+            preg_match('/^(.*?)( -[0-9]{1,3}| \+[0-9]{1,3}|)$/', $row->tol, $from);
+            preg_match('/^(.*?)( -[0-9]{1,3}| \+[0-9]{1,3}|)$/', $row->ig, $to);
             if ($to[2] == '')
                 $to[2] = '0';
             if ($from[2] == '')
                 $from[2] = '0';
-            $return[] = array('label' => preg_replace('/(' . $text . ')/i', '<b>$1</b>', $row['idoszamitas']), 'value' => $row['idoszamitas'], 'from' => $from[1], 'from2' => trim($from[2]), 'to' => $to[1], 'to2' => trim($to[2]));
+            $return[] = array('label' => preg_replace('/(' . $text . ')/i', '<b>$1</b>', $row->idoszamitas), 'value' => $row->idoszamitas, 'from' => $from[1], 'from2' => trim($from[2]), 'to' => $to[1], 'to2' => trim($to[2]));
         }
         $this->content = json_encode(array('results' => $return));
     }
