17222060f92407d5a6fe955f37c7efc3f1c3718f
borazslo <eleklaszlosj@gmail.com>
2023-08-13 22:33:58 +0200

    better docker

--- docker/entrypoint_miserend.sh
+++ docker/entrypoint_miserend.sh
@@ -4,9 +4,11 @@
 
 #docker-php-ext-install mysql mysqli pdo pdo_mysql && a2enmod rewrite && service apache2 start
 docker-php-ext-install mysql mysqli pdo pdo_mysql && a2enmod rewrite
-apt-get install sendmail -y
+
 
 apt-get update --fix-missing
+
+apt-get install sendmail -y
 apt-get install wget -y
 wget https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh
 bash install.sh
@@ -18,6 +20,13 @@ nvm install 14
 use nvm 14
 
 npm install -g
+
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+apt-get install curl php-cli php-mbstring git unzip
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
 composer install
 
 chown www-data:www-data -R /app/fajlok/tmp/
