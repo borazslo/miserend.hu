43c5f22dd5cd0bf43dfe6a37b07b7d6b932ceb22
Karoly Gossler <connor@connor.hu>
2024-01-30 12:26:29 +0100

    templom create legyen onallo methodusban

--- src/Html/Church/Create.php
+++ src/Html/Church/Create.php
@@ -9,30 +9,38 @@
 
 namespace App\Html\Church;
 
-class Create extends \App\Html\Html
+use App\Html\Html;
+use App\Model\Church;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Create extends Html
 {
-    public function __construct($path)
+    public function create(Request $request): Response
     {
-        global $user;
-        if (!$user->checkRole('miserend')) {
+        if (!$this->getSecurity()->isGranted('miserend')) {
             throw new \Exception('Nincs jogosultságod a templomot létrehozni.');
         }
 
-        $this->title = 'Új misézőhely létrehozása';
-        $this->template = 'layout.twig';
-
-        $church = new \App\Model\Church();
+        $church = new Church();
         $church->nev = 'Új misézőhely';
         $church->ok = 'n';
         $church->megbizhato = 'i';
         $church->frissites = date('Y-m-d');
         $church->moddatum = date('Y-m-d');
         $church->egyhazmegye = 1;
-        $church->save();
-        $church->nev = 'Új misézőhely - '.$church->id;
+        $church->megkozelites = '';
+        $church->plebania = '';
+        $church->leiras = '';
+        $church->megjegyzes = '';
+        $church->misemegj = '';
+        $church->bucsu = '';
+        $church->adminmegj = '';
+        $church->log = '';
         $church->save();
 
-        $this->content = "Létrehozás sikeres: <br/><a href='/templom/".$church->id."/edit'>".$church->nev.'</a>';
+        $church->nev = 'Új misézőhely - '.$church->id;
+        $church->save();
 
         $this->redirect('/templom/'.$church->id.'/edit');
     }
