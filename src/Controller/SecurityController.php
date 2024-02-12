f36c0e9c9fef57e2545b36b0d240b57a4bafd233
Karoly Gossler <connor@connor.hu>
2024-02-12 13:13:01 +0100

    [security] login oldal kinézetének javítása, bootstrap form alapok betöltése és symfony alap form téma beállítása (#203)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,25 +9,34 @@
 
 namespace App\Controller;
 
+use App\Form\Types\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/bejelentkezes', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, TranslatorInterface $translator): Response
     {
+        $form = $this->createForm(LoginType::class);
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        if ($error) {
+            $message = $translator->trans($error->getMessage(), domain: 'messages');
+            $formError = new FormError($message);
+            $form->get('_username')->addError($formError);
+        }
+
+        $form->get('_username')->setData($authenticationUtils->getLastUsername());
 
         return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
+            'form' => $form->createView(),
         ]);
     }
 
