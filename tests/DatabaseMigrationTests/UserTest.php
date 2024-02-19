406567cb28e8e21749203a628f1ae45511dd609f
Karoly Gossler <connor@connor.hu>
2024-02-19 05:25:34 +0100

    [ci] elso migteszt user load

--- tests/DatabaseMigrationTests/UserTest.php
+++ tests/DatabaseMigrationTests/UserTest.php
@@ -2,9 +2,25 @@
 
 namespace App\Tests\DatabaseMigrationTests;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class UserTest extends KernelTestCase
+class UserTest extends WebTestCase
 {
+    private UserRepository $repository;
 
+    protected function setUp(): void
+    {
+        static::bootKernel();
+        $this->repository = static::$kernel->getContainer()->get('doctrine.orm.entity_manager')->getRepository(User::class);
+    }
+
+    public function testUserLoad(): void
+    {
+        $user = $this->repository->find(1);
+
+        $this->assertSame(1, $user->getId());
+    }
 }
