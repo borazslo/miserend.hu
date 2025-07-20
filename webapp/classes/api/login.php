133332644bdb7835e3a241d4e72b07660abb47b8
borazslo <eleklaszlosj@gmail.com>
2025-07-20 21:40:25 +0200

    API user: add responsibilities

--- webapp/classes/api/login.php
+++ webapp/classes/api/login.php
@@ -9,34 +9,37 @@ class Login extends Api {
     public function docs() {
 
         $docs = [];
-        $docs['title'] = 'Felhasználó adatainak lekérdezése';
+        $docs['title'] = 'Felhasználó azonosítás';
         $docs['input'] = [
-            'token' => [
+            'username' => [
                 'required',
                 'string',
-                'Egy érvényes token'
+                'A felhasználó regisztrált neve (e-mail címmel nem működik)'
+            ],
+            'password' => [
+                'required',
+                'string',
+                'A felhasználó jelszava (egyszerű szövegként, egyszer talán lesz https)'
             ]
         ];
 
         $docs['description'] = <<<HTML
-        <p>A megfelelő url-re JSON formátumban küldött token érvényessége esetén a rendszer JSON formátumban visszaküldi a felhasználó adatait.</p>
-        <p><strong>Elérhető:</strong> <code>http://miserend.hu/api/v4/user</code></p>
+        <p>Bizonyos API funkciókhoz szükséges (pl. kedvencek szinkronizálása) vagy lehetséges (pl. visszajelzés) a felhasználó azonosítása. A megfelelő url-re JSON formátumban küldött név-jelszó páros érvényessége esetén egy token-t küld vissza a rendszer JSON formátumban.</p>
+        <p><strong>Elérhető:</strong> <code>http://miserend.hu/api/v4/login</code></p>
         HTML;
 
         $docs['response'] = <<<HTML
         <ul>
-        <li>„error": <strong>0</strong>, ha nincs hiba. <strong>1</strong>, ha van valami hiba.</li>
-        <li>„username”: A felhasználó felhasználó neve. (Soha nem üres.)</li>
-        <li>„nickname”: Becenév vagy megszólítás. (Lehetséges, hogy üres.)</li>
-        <li>„name”: Teljes név. (Lehetséges, hogy üres.)</li>
-        <li>„email”: A felhasználó email címe. (Elvileg nem lehet üres.)</li>
-        <li>„favorites": A felhasználó kedvenc templomainak azonosítóinak listája/tömbje.</li>
-        <li>„text” (opcionális): „error:1” esetén a hiba szöveges leírása.</li>
+            <li>„error”: <strong>0</strong>, ha nincs hiba. <strong>1</strong>, ha van valami hiba.</li>
+            <li>„token” (<em>varchar(32)</em>): Az azonosításhoz szükséges token. Érvényességi ideje a <code>config.php</code>-ban van beállítva.</li>
+            <li>„text” (opcionális): „error:1” esetén a hiba szöveges leírása</li>
         </ul>
         HTML;
 
         return $docs;
     }
+
+    
 
     public function validateInput() {
         if (!isset($this->input['username']) OR ! isset($this->input['password'])) {
