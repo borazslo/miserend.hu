bcf50d94ab262504b9a225fb30ef89c7209c5597
borazslo <eleklaszlosj@gmail.com>
2026-01-02 23:28:30 +0100

    better error message when loadError of calendar api by angular

--- calendar/src/app/components/church/church.component.ts
+++ calendar/src/app/components/church/church.component.ts
@@ -1,4 +1,5 @@
 import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {ChurchCalendarComponent} from '../church-calendar/church-calendar.component';
 import {Church} from '../../model/church';
 import {Mass} from '../../model/mass';
@@ -9,6 +10,7 @@ import {SpinnerService} from '../../services/spinner.service';
 @Component({
   selector: 'app-church',
   imports: [
+    CommonModule,
     ChurchCalendarComponent
   ],
   templateUrl: './church.component.html',
@@ -19,6 +21,7 @@ export class ChurchComponent implements OnInit {
   public dataLoaded: boolean = false;
   public currentChurch?: Church;
   public masses: Map<number, Mass> = new Map();
+  public loadError: string | null = null;
 
   @ViewChild('churchCalendar') churchCalendar!: ChurchCalendarComponent;
 
@@ -36,10 +39,23 @@ export class ChurchComponent implements OnInit {
 
   private initEvents() {
     const churchId: number = +this.activatedRoute.snapshot.params['id'];
-    this.eventService.getChurch(churchId).subscribe((church: Church) => {
-      this.currentChurch = church;
-      this.masses = new Map(church.masses.map(e => [e.id!, e]));
-      this.dataLoaded = true;
+    this.eventService.getChurch(churchId).subscribe({
+      next: (church: Church) => {
+        this.currentChurch = church;
+        this.masses = new Map(church.masses.map(e => [e.id!, e]));
+        this.loadError = null;
+        this.dataLoaded = true;
+        this.spinnerService.hide();
+      },
+      error: (err: any) => {
+        console.error('Failed to load church data', err);
+        // Show a concise, user-friendly error message. Keep dataLoaded true so template can render the message.
+        this.loadError = 'Nem sikerült betölteni a templom adatait, mert az adatszolgáltató oldal nem elérhető. Elnézést kérünk.';
+        this.currentChurch = undefined;
+        this.masses = new Map();
+        this.dataLoaded = true;
+        this.spinnerService.hide();
+      }
     });
   }
 
